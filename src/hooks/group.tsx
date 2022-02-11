aaa58b580e2e04d52b8074980daa6fd07020368b
Vadim Egorov <43100771+vadimegorov13@users.noreply.github.com>
2022-02-10 19:58:59 -0900

    create group, admin, participant, and message collections

--- src/hooks/group.tsx
+++ src/hooks/group.tsx
@@ -1,4 +1,4 @@
-import uniqid from 'uniqid';
+import { serverTimestamp } from 'firebase/firestore';
 import { db } from '../firebase/firebase';
 import { GroupData, GroupForm, useGroupType } from '../utils/types';
 import { useAuth } from './useAuth';
@@ -6,36 +6,47 @@ import { useAuth } from './useAuth';
 export const useGroup = (): useGroupType => {
   const { user } = useAuth();
 
+  /**
+   *
+   * @param {GroupForm} group
+   * @return {Promise<GroupData | any>}
+   *
+   * Creates group
+   * Creates new 'admins' doc with the same is as the group
+   * Creates new 'participants' doc with the same is as the group
+   * Creates new 'messages' doc with the same is as the group
+   *
+   */
   const createGroup = async (group: GroupForm): Promise<GroupData | any> => {
     if (user) {
-      const groupData: GroupData = {
-        id: uniqid(),
-        name: group.name,
-        description: group.description,
-      };
-
       try {
-        await db.collection('groups').doc(groupData.id).set(groupData);
-        return groupData;
+        // Create a group doc with randomly generated id
+        const newGroup = db.collection('groups').doc();
+
+        // Set values to the group
+        const res = await newGroup.set({
+          id: newGroup.id,
+          name: group.name,
+          description: group.description,
+          createdAt: serverTimestamp(),
+          updatedAt: serverTimestamp(),
+        });
+
+        // Create admins doc with the same id as the group
+        db.collection('admins').doc(newGroup.id).set({ userId: user.id });
+
+        // Create participants doc with the same id as the group
+        db.collection('participants').doc(newGroup.id).set({});
+
+        // Create messages doc with the same id as the group
+        db.collection('messages').doc(newGroup.id).set({});
+
+        return res;
       } catch (error) {
         return { error };
       }
     }
   };
-
-  // const getGroups = async () => {
-  //   const snapshot = await db.collection('groups').get();
-
-  //   const groupList: Array<GroupData> = snapshot.docs.map((group) => {
-  //     return {
-  //       id: group.id,
-  //       name: group.data().name,
-  //       description: group.data().description,
-  //     };
-  //   });
-
-  //   return groupList;
-  // };
 
   return {
     // getGroups,
