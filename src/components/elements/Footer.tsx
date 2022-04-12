9ddd1dbf39d6a7fad94b57234a6f54368cd83a46
Vadim Egorov <43100771+vadimegorov13@users.noreply.github.com>
2022-04-12 14:25:19 -0800

    footer

--- src/components/elements/Footer.tsx
+++ src/components/elements/Footer.tsx
@@ -1,7 +1,10 @@
+import { UnstyledButton } from '@mantine/core';
 import FavoriteIcon from '@mui/icons-material/Favorite';
-import Box from '@mui/material/Box';
-import Container from '@mui/material/Container';
-import Typography from '@mui/material/Typography';
+import GitHubIcon from '@mui/icons-material/GitHub';
+import LinkedInIcon from '@mui/icons-material/LinkedIn';
+import PollIcon from '@mui/icons-material/Poll';
+import { Box, Container, Typography, Divider, Grid } from '@mui/material';
+import Link from 'next/link';
 
 const Footer = () => {
   return (
@@ -15,10 +18,71 @@ const Footer = () => {
     >
       <Box component="footer">
         <Container maxWidth="sm">
-          <Typography variant="body1">
-            Build with <FavoriteIcon color="primary" /> by PomoFriends Team
+          <Typography variant="body1" mb={2}>
+            Build with <FavoriteIcon color="primary" fontSize="small" /> by
+            PomoFriends Team
           </Typography>
         </Container>
+        <Divider />
+        <Grid container spacing={2} mt={2}>
+          <Grid item xs={4}>
+            <UnstyledButton>
+              <Link
+                href="https://github.com/PomoFriends/pomofriends-app"
+                passHref
+              >
+                <Box
+                  sx={{
+                    display: 'flex',
+                    alignItems: 'center',
+                    flexWrap: 'wrap',
+                  }}
+                >
+                  <GitHubIcon color="primary" />
+                  <Typography component={'span'} color="white" ml={0.5}>
+                    GitHub
+                  </Typography>
+                </Box>
+              </Link>
+            </UnstyledButton>
+          </Grid>
+          <Grid item xs={4}>
+            <UnstyledButton>
+              <Link href="https://www.linkedin.com/in/vadimegorov13/" passHref>
+                <Box
+                  sx={{
+                    display: 'flex',
+                    alignItems: 'center',
+                    flexWrap: 'wrap',
+                  }}
+                >
+                  <LinkedInIcon color="primary" />
+                  <Typography component={'span'} color="white" ml={0.5}>
+                    LinkedIn
+                  </Typography>
+                </Box>
+              </Link>
+            </UnstyledButton>
+          </Grid>
+          <Grid item xs={4}>
+            <UnstyledButton>
+              <Link href="/" passHref>
+                <Box
+                  sx={{
+                    display: 'flex',
+                    alignItems: 'center',
+                    flexWrap: 'wrap',
+                  }}
+                >
+                  <PollIcon color="primary" />
+                  <Typography component={'span'} color="white" ml={0.5}>
+                    Survey
+                  </Typography>
+                </Box>
+              </Link>
+            </UnstyledButton>
+          </Grid>
+        </Grid>
       </Box>
     </Box>
   );
